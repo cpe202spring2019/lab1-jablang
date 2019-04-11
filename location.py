1d06dd2aaa4067f4160cdf30f7ca16d0d7cd8699
jablang <49251460+jablang@users.noreply.github.com>
2019-04-10 22:45:32 -0700

    Update location.py

--- location.py
+++ location.py
@@ -7,14 +7,15 @@ class Location:
         self.lat = lat      # latitude in degrees (-90 to 90)
         self.lon = lon      # longitude in degrees (-180 to 180)
 
-    def __eq__(self,other):
-        return((type(other) == Location) and
-               self.name == other.name and
-               self.lat == other.lat and
-               self.lon == self.lon
-               )
+    def __eq__(self, other):
+        return ((type(other) == Location)
+                and self.name == other.name
+                and self.lat == other.lat
+                and self.lon == other.lon
+                )
+
     def __repr__(self):
-        return ("Location({!s},{!s},{!s})".format(self.name, self.lat, self.lon))
+        return ("Location({!s}, {!s}, {!s})".format(self.name, self.lat, self.lon))
 
 def main():
     loc1 = Location("SLO", 35.3, -120.7)
