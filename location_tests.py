b1c2ce69b4107f2151f992470c1b72be3582b040
jablang <49251460+jablang@users.noreply.github.com>
2019-04-10 22:48:57 -0700

    Update location_tests.py

--- location_tests.py
+++ location_tests.py
@@ -1,28 +1,27 @@
 import unittest
 from location import *
 
-class TestLab1(unittest.TestCase):
+class TestLab0(unittest.TestCase):
 
-    def test_repr(self):
-        loc = Location("SLO", 35.3, -120.7)
-        self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
-    def test_repr(self):
-        loc1 = Location("Stanton", 33.8, -118.0)
-        self.assertEqual(repr(loc1),"Location('Stanton', 33.8, -118.0)")
-
-    
     def test_eq(self):
         loc = Location("SLO", 35.3, -120.7)
-        loc1 = Location("SLO", 35.3, -120.7) 
+        loc1 = Location("SLO", 35.3, -120.7)
         self.assertTrue(loc == loc1)
-    
-    def test_eq2(self):
-        loc = Location("SLO", 35.3, -120.7)
-        loc1 = LocationLocation("Stanton", 33.8, -118.0) 
-        self.assertFalse(loc == loc1)
-        
-    
 
+    def test_eq1(self):
+        loc = Location("SLO", 35.3, -120.7)
+        loc1 = Location("North Pole", 90.0, -135.0)
+        self.assertFalse(loc == loc1)
+
+    def test_repr(self):
+        loc = Location("SLO", 35.3, -120.7)
+        self.assertEqual(repr(loc), "Location(SLO,35.3,-120.7)")
+
+    def test_repr1(self):
+        loc1 = Location("North Pole", 90.0, -135.0)
+        self.assertEqual(repr(loc1), "Location(North Pole,90.0,-135.0)")
+
+       
+        
 if __name__ == "__main__":
         unittest.main()
